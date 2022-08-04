5d7948b7fd0fdeab85005814a232f4f6ff6063ae
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-08-04 19:37:39 -0300

    feature/controla componente input

--- src/components/campo de busca/CampoDeBusca.jsx
+++ src/components/campo de busca/CampoDeBusca.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSearchSharp } from 'react-icons/io5'
 import './CampoDeBuscaStyle.css'
 
 export default function CampoDeBusca () {
+  const [usuario, setUsuario] = useState('')
+
+  function mudaEstado ({ target }) {
+    setUsuario(target.value)
+    console.log(usuario)
+  }
   return (
     <div className="centralize container">
       <p className="titulo-da-busca display-6">Busque o usuario no github</p>
       <div className="container-busca">
         <label className="visually-hidden">Usuario do git</label>
-        <input type="password" className="form-control" id="inputPassword2" placeholder="Digite o nome do usuario "></input>
+        <input type="text" onChange= { mudaEstado } className="form-control" id="pesquisa" placeholder="Digite o nome do usuario "></input>
         <button type="button" className="btn-pesquisa btn btn-secondary"><IoSearchSharp /><span className='btn-texto'>Buscar</span></button>
         <i className="bi bi-1-circle"></i>
       </div>
